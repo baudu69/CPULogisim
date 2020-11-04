f68c7e1dacfb852c0c13606fe990d4730771705d
baudu <bastien.audu@gmail.com>
2020-11-04 10:02:08 +0100

    2 premiers prog de test

---
+++ Assembleur/prog1.asm
@@ -0,0 +1,13 @@
+;Programme de calcul de factoriel
+;R0 : entree prog
+;R1 : Resultat
+;R2 : Constant = a 1
+start: LDvi R0 5
+    LDvi R1 1
+    LDvi R2 1
+    CALL facto
+    STOP
+facto: MUL R1 R1 R0
+    SUBi R0 R0 1
+    JGRA R0 R2 facto
+    RET
